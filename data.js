d36f670ec194e0d154313feccd4f4a2bd599ac44
Maximo Ponce <mponce8979@gmail.com>
2022-08-07 21:20:55 -0400

    javascript updated to include DIFFICULTY settings

--- data.js
+++ data.js
@@ -3,17 +3,16 @@ const easy = {
     desc: "3 tries with a slow moving ball!",
     color: "#a8ff65",
     health: "3",
-    speed: "5",
-    points: "5"
+    speed: 5,
+    points: 5
 }
-
 const medium = {
     diff: "medium",
     desc: "2 tries with a normal speed ball!",
     color: "#fff53b",
     health: "2",
-    speed: "6.5",
-    points: "15",
+    speed: 6.5,
+    points: 15,
 }   
 
 const hard = {
@@ -21,8 +20,8 @@ const hard = {
     desc: "1 life, fast ball. can you do it?",
     color: "#ff0000",
     health:"1",
-    speed:"8",
-    points: "25",
+    speed:8,
+    points: 25,
 }
 
 let difficulties = [easy, medium, hard];
--- main.js
+++ main.js
@@ -1,22 +1,77 @@
 
+let ebtn = document.querySelector("#easy");
+let mbtn = document.querySelector("#medium");
+let hbtn = document.querySelector("#hard");
 
+let btns = [ebtn, mbtn, hbtn];
 
-let diffs = difficulties;
+for (i = 0; i < btns.length; i++) {
 
-let r = 255
-let g = 255
-let b = 255
+    if (i == 0) {
+        btns[i].style ["background-color"] = [`${easy.color}`];
+        
+    } else if (i == 1) {
+        btns[i].style ["background-color"] = [`${medium.color}`];
+    } else {
+        btns[i].style ["background-color"] = [`${hard.color}`];
+    }
+
+}
+let r = 255;
+let g = 255;
+let b = 255;
 var ballX;
 var ballY;
-var ballSpeedX = 8;
-var ballSpeedY = 8;
+let ballSpeedX = 0;
+let ballSpeedY = 0;
 var paddleHeight = 40;
 var length = 200;
-var backgroundColor = 128
+var backgroundColor = 128;
 let points = 0;
+let health = 0;
+let pointInc = 0;
 
+function changeVarE() {
+    console.log("I detected easy");
+    ballX = 0;
+    ballY = 0;
+    health = 0;
+    health = easy.health;
+    points = 0;
+    pointInc = easy.points;
+    ballSpeedX = easy.speed;
+    ballSpeedY = easy.speed;
+} 
+
+function changeVarM() {
+    console.log("I detected easy");
+    ballX = 0;
+    ballY = 0;
+    health = 0;
+    health = medium.health;
+    points = 0;
+    pointInc = medium.points;
+    ballSpeedX = medium.speed;
+    ballSpeedY = medium.speed;
+} 
+
+function changeVarH() {
+    console.log("I detected easy");
+    ballX = 0;
+    ballY = 0;
+    health = 0;
+    health = hard.health;
+    points = 0
+    pointInc = hard.points;
+    ballSpeedX = hard.speed;
+    ballSpeedY = hard.speed;
+} 
+
+ebtn.addEventListener("click", changeVarE);
+mbtn.addEventListener("click", changeVarM);
+hbtn.addEventListener("click", changeVarH);
 function setup() {
-    let div = document.getElementById("main_div");
+
   createCanvas(600,600);
   noCursor();
   ballX = random(width)
@@ -29,6 +84,8 @@ function draw() {
    fill(255, 255, 255);
    textSize(22);
    text("Score: " + points, 30, 30);
+   text("Health: " + health, 30, 50);
+
   strokeWeight(4);
   stroke(255);
   fill(255);
@@ -51,12 +108,15 @@ function draw() {
       r = random(0,256);
       g = random(0,256);
       b = random(0,256);
-      points += 25
+      points += pointInc
     }
     else {
-        health--;
-        if (health = 0) {  
-            background(255,0,0)
+        health -= 1;
+        ballX = 0;
+        ballY = 0;
+        
+        if (health === 0) {  
+            background(255,0,0);
             fill(0);
             text("GAME OVER!", width / 2, height / 2);
             noLoop();
